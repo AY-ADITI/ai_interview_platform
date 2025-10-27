371d50568a668e95e027ade436ebbcfab5969a60
Aditi Yadav <arpanayadav350@gmail.com>
2025-10-28 02:25:14 +0530

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint:{
+    ignoreDuringBuilds:process.env.NODE_ENV === "production" ? false : true,
+  },
+  typescript:{
+    ignoreBuildErrors:false,
+  }
+};
+
+export default nextConfig;
